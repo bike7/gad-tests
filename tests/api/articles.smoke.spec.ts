9dea6cf5a798cf9c800cc8369fe96bb35745f120
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-06 10:32:17 +0200

    test: api smoke articles endpoint

---
+++ tests/api/articles.smoke.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify articles API endpoint', () => {
+  test('GET articles returns status code 200 @api', async ({ request }) => {
+    // Arrange
+    const articlesEndpoint = '/api/articles';
+    const expectedResponseStatusCode = 200;
+    // Act
+    const response = await request.get(articlesEndpoint);
+    // Assert
+    expect(response.status()).toBe(expectedResponseStatusCode);
+  });
+});
