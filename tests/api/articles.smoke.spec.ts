d7472252ab4b132f9c627c0c7e2967343e947c81
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-06 11:26:15 +0200

    test: min number of returned articles

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,15 +1,26 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
-test.describe('Verify articles API endpoint', () => {
-  test('GET articles returns status code 200 @GAD-R08-01 @api', async ({
-    request,
-  }) => {
+test.describe('Verify articles API endpoint @GAD-R08-01 @api', () => {
+  const articlesEndpoint = '/api/articles';
+
+  test('GET articles should return status code 200', async ({ request }) => {
     // Arrange
-    const articlesEndpoint = '/api/articles';
     const expectedResponseStatusCode = 200;
     // Act
     const response = await request.get(articlesEndpoint);
     // Assert
     expect(response.status()).toBe(expectedResponseStatusCode);
+  });
+
+  test('GET articles should return at least one article @predefined_data', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedMinArticlesCount = 1;
+    // Act
+    const response = await request.get(articlesEndpoint);
+    const articles = await response.json();
+    // Assert
+    expect(articles.length).toBeGreaterThanOrEqual(expectedMinArticlesCount);
   });
 });
