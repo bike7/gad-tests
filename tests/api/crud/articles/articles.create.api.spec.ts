cea8f67bc55297a69249d3b4752d2f143aa40c4c
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-07 16:53:35 +0200

    test: create an article with put

--- tests/api/crud/articles/articles.create.api.spec.ts
+++ tests/api/crud/articles/articles.create.api.spec.ts
@@ -1,10 +1,16 @@
 import { createArticleViaApi } from '@_src/api/factories/article-create.api.factory';
 import { prepareArticlePayload } from '@_src/api/factories/article-payload.api.factory';
 import { getAuthorizationHeader } from '@_src/api/factories/authorization-header.api.factory';
+import { Headers } from '@_src/api/models/headers.api.model';
 import { apiEndpoints } from '@_src/api/utils/api.util';
 import { expect, test } from '@_src/ui/fixtures/merge.fixture';
 
 test.describe('Verify articles CREATE operations @GAD-R09-01 @crud @create @articles', () => {
+  let authorizationHeader: Headers;
+  test.beforeAll('Should login', async ({ request }) => {
+    authorizationHeader = await getAuthorizationHeader(request);
+  });
+
   test('Should not create an article with a non-logged user', async ({
     request,
   }) => {
@@ -25,7 +31,6 @@ test.describe('Verify articles CREATE operations @GAD-R09-01 @crud @create @arti
   test('Should create an article with a logged user', async ({ request }) => {
     // Arrange
     const expectedResponseStatus = 201;
-    const authorizationHeader = await getAuthorizationHeader(request);
     const articleData = prepareArticlePayload();
     // Act
     const articleResponse = await createArticleViaApi(
@@ -39,5 +44,26 @@ test.describe('Verify articles CREATE operations @GAD-R09-01 @crud @create @arti
     expect.soft(actualResponseStatus).toBe(expectedResponseStatus);
     expect.soft(actualResponseBody.title).toBe(articleData.title);
     expect.soft(actualResponseBody.body).toBe(articleData.body);
+  });
+
+  test('Should create a new article when modified article id does not exist with a logged user', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedResponseStatus = 201;
+    const endpoint = `${apiEndpoints.articles}/${new Date().valueOf()}`;
+    const articleData = prepareArticlePayload();
+    // Act
+    const response = await request.put(endpoint, {
+      headers: authorizationHeader,
+      data: articleData,
+    });
+    const article = await response.json();
+    // Assert
+    expect(response.status()).toBe(expectedResponseStatus);
+    expect.soft(article.title).toBe(articleData.title);
+    expect.soft(article.body).toBe(articleData.body);
+    expect.soft(article.date).toBe(articleData.date);
+    expect.soft(article.image).toBe(articleData.image);
   });
 });
