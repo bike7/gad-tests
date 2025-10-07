4f94f6337c8c8a02a6814c741c0ffd7f3bc70a8f
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-07 18:02:08 +0200

    tests: modify articles using patch

--- tests/api/crud/articles/articles.modify.api.spec.ts
+++ tests/api/crud/articles/articles.modify.api.spec.ts
@@ -6,7 +6,7 @@ import { apiEndpoints } from '@_src/api/utils/api.util';
 import { expect, test } from '@_src/ui/fixtures/merge.fixture';
 import { APIResponse } from '@playwright/test';
 
-test.describe('Verify articles UPDATE operations @GAD-R10-01 @crud @delete @articles', () => {
+test.describe('Verify articles UPDATE operations @crud @update @articles', () => {
   let authorizationHeader: Headers;
   let createdArticleResponse: APIResponse;
 
@@ -23,62 +23,156 @@ test.describe('Verify articles UPDATE operations @GAD-R10-01 @crud @delete @arti
     );
   });
 
-  test('Should modify an article with a logged user', async ({ request }) => {
-    // Arrange
-    const expectedResponseStatus = 200;
-    const createdArticle = await createdArticleResponse.json();
-    const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
-    const modifiedArticleData = prepareArticlePayload();
-    const image =
-      '.\\data\\images\\256\\rory-mckeever-w0kMi0osklU-unsplash.jpg';
-    modifiedArticleData.image = image;
-    // Act
-    const response = await request.put(createdArticleEndpoint, {
-      headers: authorizationHeader,
-      data: modifiedArticleData,
+  test.describe('Fully modify articles @GAD-R10-01', () => {
+    test('Should fully modify an article with a logged user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedResponseStatus = 200;
+      const createdArticle = await createdArticleResponse.json();
+      const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
+      const modifiedArticleData = prepareArticlePayload();
+      const image =
+        '.\\data\\images\\256\\rory-mckeever-w0kMi0osklU-unsplash.jpg';
+      modifiedArticleData.image = image;
+      // Act
+      const response = await request.put(createdArticleEndpoint, {
+        headers: authorizationHeader,
+        data: modifiedArticleData,
+      });
+      const modifiedArticle = await response.json();
+      // Assert
+      expect(response.status()).toBe(expectedResponseStatus);
+      expect.soft(modifiedArticle.id).toBe(createdArticle.id);
+      expect.soft(modifiedArticle.title).toBe(modifiedArticleData.title);
+      expect.soft(modifiedArticle.title).not.toBe(createdArticle.title);
+      expect.soft(modifiedArticle.body).toBe(modifiedArticleData.body);
+      expect.soft(modifiedArticle.body).not.toBe(createdArticle.body);
+      expect.soft(modifiedArticle.date).toBe(modifiedArticleData.date);
+      expect.soft(modifiedArticle.date).not.toBe(createdArticle.date);
+      expect.soft(modifiedArticle.image).toBe(modifiedArticleData.image);
+      expect.soft(modifiedArticle.image).not.toBe(createdArticle.image);
     });
-    const modifiedArticle = await response.json();
-    // Assert
-    expect(response.status()).toBe(expectedResponseStatus);
-    expect.soft(modifiedArticle.id).toBe(createdArticle.id);
-    expect.soft(modifiedArticle.title).toBe(modifiedArticleData.title);
-    expect.soft(modifiedArticle.title).not.toBe(createdArticle.title);
-    expect.soft(modifiedArticle.body).toBe(modifiedArticleData.body);
-    expect.soft(modifiedArticle.body).not.toBe(createdArticle.body);
-    expect.soft(modifiedArticle.date).toBe(modifiedArticleData.date);
-    expect.soft(modifiedArticle.date).not.toBe(createdArticle.date);
-    expect.soft(modifiedArticle.image).toBe(modifiedArticleData.image);
-    expect.soft(modifiedArticle.image).not.toBe(createdArticle.image);
+
+    test('Should not fully modify an article with a non-logged user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedResponseStatus = 401;
+      const expectedErrorMessage = 'Access token not provided!';
+      const createdArticle = await createdArticleResponse.json();
+      const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
+      const modifiedArticleData = prepareArticlePayload();
+      const image =
+        '.\\data\\images\\256\\rory-mckeever-w0kMi0osklU-unsplash.jpg';
+      modifiedArticleData.image = image;
+      // Act
+      const response = await request.put(createdArticleEndpoint, {
+        data: modifiedArticleData,
+      });
+      const responseBody = await response.json();
+      const nonModifiedArticleResponse = await request.get(
+        createdArticleEndpoint,
+      );
+      const nonModifiedArticle = await nonModifiedArticleResponse.json();
+      // Assert
+      expect.soft(response.status()).toBe(expectedResponseStatus);
+      expect.soft(responseBody.error.message).toBe(expectedErrorMessage);
+      expect.soft(nonModifiedArticle.id).toBe(createdArticle.id);
+      expect.soft(nonModifiedArticle.title).toBe(createdArticle.title);
+      expect.soft(nonModifiedArticle.body).toBe(createdArticle.body);
+      expect.soft(nonModifiedArticle.date).toBe(createdArticle.date);
+      expect.soft(nonModifiedArticle.image).toBe(createdArticle.image);
+    });
   });
 
-  test('Should not modify an article with a non-logged user', async ({
-    request,
-  }) => {
-    // Arrange
-    const expectedResponseStatus = 401;
-    const expectedErrorMessage = 'Access token not provided!';
-    const createdArticle = await createdArticleResponse.json();
-    const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
-    const modifiedArticleData = prepareArticlePayload();
-    const image =
-      '.\\data\\images\\256\\rory-mckeever-w0kMi0osklU-unsplash.jpg';
-    modifiedArticleData.image = image;
-    // Act
-    const response = await request.put(createdArticleEndpoint, {
-      data: modifiedArticleData,
+  test.describe('Partially modify articles @GAD-R10-03', () => {
+    test('Should partially modify an article with a logged user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedResponseStatus = 200;
+      const createdArticle = await createdArticleResponse.json();
+      const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
+      const modifiedArticleData = {
+        title: `Patched Title no. ${new Date().getDate().valueOf()}`,
+      };
+      // Act
+      const response = await request.patch(createdArticleEndpoint, {
+        headers: authorizationHeader,
+        data: modifiedArticleData,
+      });
+      const modifiedArticle = await response.json();
+      // Assert
+      expect(response.status()).toBe(expectedResponseStatus);
+      expect.soft(modifiedArticle.id).toBe(createdArticle.id);
+      expect.soft(modifiedArticle.title).toBe(modifiedArticleData.title);
+      expect.soft(modifiedArticle.title).not.toBe(createdArticle.title);
+      expect.soft(modifiedArticle.body).toBe(createdArticle.body);
+      expect.soft(modifiedArticle.date).toBe(createdArticle.date);
+      expect.soft(modifiedArticle.image).toBe(createdArticle.image);
     });
-    const responseBody = await response.json();
-    const nonModifiedArticleResponse = await request.get(
-      createdArticleEndpoint,
-    );
-    const nonModifiedArticle = await nonModifiedArticleResponse.json();
-    // Assert
-    expect.soft(response.status()).toBe(expectedResponseStatus);
-    expect.soft(responseBody.error.message).toBe(expectedErrorMessage);
-    expect.soft(nonModifiedArticle.id).toBe(createdArticle.id);
-    expect.soft(nonModifiedArticle.title).toBe(createdArticle.title);
-    expect.soft(nonModifiedArticle.body).toBe(createdArticle.body);
-    expect.soft(nonModifiedArticle.date).toBe(createdArticle.date);
-    expect.soft(nonModifiedArticle.image).toBe(createdArticle.image);
+
+    test('Should not partially modify an article with a non-logged user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedResponseStatus = 401;
+      const expectedErrorMessage = 'Access token not provided!';
+      const createdArticle = await createdArticleResponse.json();
+      const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
+      const modifiedArticleData = {
+        title: `Patched Title no. ${new Date().getDate().valueOf()}`,
+      };
+      // Act
+      const response = await request.patch(createdArticleEndpoint, {
+        data: modifiedArticleData,
+      });
+      const responseBody = await response.json();
+      const nonModifiedArticleResponse = await request.get(
+        createdArticleEndpoint,
+      );
+      const nonModifiedArticle = await nonModifiedArticleResponse.json();
+      // Assert
+      expect.soft(response.status()).toBe(expectedResponseStatus);
+      expect.soft(responseBody.error.message).toBe(expectedErrorMessage);
+      expect.soft(nonModifiedArticle.id).toBe(createdArticle.id);
+      expect.soft(nonModifiedArticle.title).toBe(createdArticle.title);
+      expect.soft(nonModifiedArticle.body).toBe(createdArticle.body);
+      expect.soft(nonModifiedArticle.date).toBe(createdArticle.date);
+      expect.soft(nonModifiedArticle.image).toBe(createdArticle.image);
+    });
+
+    test('Should not partially modify an article with improper field and logged user', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedResponseStatus = 422;
+      const nonExistingField = 'nonExistingField';
+      const expectedErrorMessage = `One of field is invalid (empty, invalid or too long) or there are some additional fields: Field validation: "${nonExistingField}" not in [id,user_id,title,body,date,image]`;
+      const modifiedArticleData = {
+        [nonExistingField]: 'non existing value',
+      };
+      const createdArticle = await createdArticleResponse.json();
+      const createdArticleEndpoint = `${apiEndpoints.articles}/${createdArticle.id}`;
+      // Act
+      const response = await request.patch(createdArticleEndpoint, {
+        headers: authorizationHeader,
+        data: modifiedArticleData,
+      });
+      const responseBody = await response.json();
+      const nonModifiedArticleResponse = await request.get(
+        createdArticleEndpoint,
+      );
+      const nonModifiedArticle = await nonModifiedArticleResponse.json();
+      // Assert
+      expect.soft(response.status()).toBe(expectedResponseStatus);
+      expect.soft(responseBody.error.message).toBe(expectedErrorMessage);
+      expect.soft(nonModifiedArticle.id).toBe(createdArticle.id);
+      expect.soft(nonModifiedArticle.title).toBe(createdArticle.title);
+      expect.soft(nonModifiedArticle.body).toBe(createdArticle.body);
+      expect.soft(nonModifiedArticle.date).toBe(createdArticle.date);
+      expect.soft(nonModifiedArticle.image).toBe(createdArticle.image);
+    });
   });
 });
