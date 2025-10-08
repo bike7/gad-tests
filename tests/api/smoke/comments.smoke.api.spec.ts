1d75e50b83e6fe54a0e2922e3fe8bb39f3d41c2b
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-08 14:05:23 +0200

    chore: update comments smoke tests

--- tests/api/smoke/comments.smoke.api.spec.ts
+++ tests/api/smoke/comments.smoke.api.spec.ts
@@ -1,30 +1,31 @@
-import { apiEndpoints } from '@_src/api/utils/api.util';
 import { expect, test } from '@_src/merge.fixture';
 
 test.describe('Verify comments API endpoint @GAD-R08-02 @smoke @comments', () => {
-  test('GET comments should return status code 200', async ({ request }) => {
+  test('GET comments should return status code 200', async ({
+    commentsRequest,
+  }) => {
     // Arrange
     const expectedResponseStatusCode = 200;
     // Act
-    const response = await request.get(apiEndpoints.comments);
+    const response = await commentsRequest.get();
     // Assert
     expect(response.status()).toBe(expectedResponseStatusCode);
   });
 
   test('GET comments should return at least one comment @predefined_data', async ({
-    request,
+    commentsRequest,
   }) => {
     // Arrange
     const expectedMinCommentsCount = 1;
     // Act
-    const response = await request.get(apiEndpoints.comments);
+    const response = await commentsRequest.get();
     const comments = await response.json();
     // Assert
     expect(comments.length).toBeGreaterThanOrEqual(expectedMinCommentsCount);
   });
 
   test('GET comments should return comment object @predefined_data', async ({
-    request,
+    commentsRequest,
   }) => {
     // Arrange
     const expectedRequiredFields = [
@@ -35,7 +36,7 @@ test.describe('Verify comments API endpoint @GAD-R08-02 @smoke @comments', () =>
       'date',
     ];
     // Act
-    const response = await request.get(apiEndpoints.comments);
+    const response = await commentsRequest.get();
     const comments = await response.json();
     const firstComment = comments[0];
     // Assert
