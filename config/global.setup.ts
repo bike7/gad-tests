ca0f43ce0a0ecb9844dfaa061ce3580c6c7f6faf
Katarzyna Pytel <pytel.kat@gmail.com>
2025-10-08 20:22:53 +0200

    chore: added basic health check to global config

--- config/global.setup.ts
+++ config/global.setup.ts
@@ -1,9 +1,19 @@
+import { BASE_URL } from '@_config/env.config';
 import { STORAGE_STATE } from '@_pw-config';
+import { request } from '@playwright/test';
 import * as fs from 'fs';
 
 async function globalSetup(): Promise<void> {
   if (fs.existsSync(STORAGE_STATE)) {
     fs.unlinkSync(STORAGE_STATE);
+  }
+  const requestContext = await request.newContext();
+  try {
+    await requestContext.get(BASE_URL);
+  } catch {
+    throw new Error(
+      `❌ Failed to connect to ${BASE_URL!}. Please make sure the application is running and the baseUrl is correct`,
+    );
   }
 }
 
